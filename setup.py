2c6cdadada6d115318bd120ab5214596c61808be
Ammar Nasr <ammarnasraza@gmail.com>
2024-02-23 13:06:59 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="enigma_ai", 
-    version="0.2.1",
+    version="0.2.2",
     description="Tools for simple and efficient training of LLMs for code generation",
     long_description=pathlib.Path("README.md").read_text(),
     long_description_content_type="text/markdown",
